ffac6f88bfc1d257e2bae15b1fe5d4857f012f88
Steven Prindle <flankbusiness@gmail.com>
2020-06-12 01:23:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,3 +6,9 @@ school.roster
 school.add_student("Zach Morris", 9)
 school.roster
 # => {9 => ["Zach Morris"]}
+
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+
+hash
+ => {"new_key"=>["new_value_for_value_array"]}
